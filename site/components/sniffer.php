6d605d4d76071c249848b6070aff351bd0229355
Ben5 <ben-smith@blueyonder.co.uk>
2013-02-13 12:45:58 +0000

    switching to reading the embed-able version of the Sniffer output file, which reads in the external stylesheet and embeds it as inline style tag.

--- site/components/sniffer.php
+++ site/components/sniffer.php
@@ -9,12 +9,23 @@ class Sniffer extends ComponentBase
     protected function
     Index()
     {
-        $fileName = '/opt/git/Sniffer/output/sniffer.html';
+        $fileName = '/opt/git/Sniffer/output/sniffer_embed.html';
 
         if (is_readable($fileName))
         {
             $snifferFile = fopen($fileName, 'r');
             $output = fread($snifferFile, filesize($fileName));
+
+            // now read in the stylesheet, ready for prepending
+            $stylesheetFilename = '/opt/git/Sniffer/output/output.css';
+
+            if (is_readable($stylesheetFilename))
+            {
+                $styleFile = fopen($stylesheetFilename, 'r');
+                $styleString = fread($styleFile, filesize($stylesheetFilename));
+
+                $output = '<style type="text/css">' . $styleString . '</style>' . $output;
+            }
         }
         else
         {
