d56fc28abc1a8a93b5c5217affffcc5e26781dfc
Ben5 <ben-smith@blueyonder.co.uk>
2013-02-14 09:10:49 +0000

    adding a link to my GitHub page to the About Me section

--- site/views/about.php
+++ site/views/about.php
@@ -10,7 +10,7 @@
         </ul>
         <h4>Interests:</h4>
         <ul>
-            <li><b>Programming</b> - I enjoy writing code and playing around with new technologies in my spare time. I have recently been investigating the Nginx web server as an alternative to Apache, and I have been working with components from the Symfony2 PHP framework to replace my custom MVC implementation. A selection of my projects is available on my GitHub page.</li>
+            <li><b>Programming</b> - I enjoy writing code and playing around with new technologies in my spare time. I have recently been investigating the Nginx web server as an alternative to Apache, and I have been working with components from the Symfony2 PHP framework to replace my custom MVC implementation. A selection of my projects is available on my <a href="https://github.com/Ben5" target="__blank">GitHub page</a>.</li>
             <li><b>Playing video games</b> - I am a huge fan of video games, with my favourite genres being racing and RPGs, although I enjoy anything that I can play with friends, either cooperatively or competitively.</li>
             <li><b>Driving</b> - I love cars and I love driving. In recent years, I have been to a number of track day events, as well as a stunt-driving course. I am looking forward to attending an off-road driving class later this year.</li>
             <li><b>Playing guitar</b> - I am attempting to teach myself to play guitar. It's slow-going, but I am getting a lot of satisfaction from experimenting and picking things up as I go along.</li>
