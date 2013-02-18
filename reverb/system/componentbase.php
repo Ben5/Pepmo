9e73fcf5252de4e0e2251da0d28864079d02035a
Ben5 <ben-smith@blueyonder.co.uk>
2013-02-18 11:34:10 +0000

    (change from REVERB) component actions can now select which view template to render, instead of having all actions on a component render the same view file (optional)

--- reverb/system/componentbase.php
+++ reverb/system/componentbase.php
@@ -6,11 +6,13 @@ class ComponentBase
 {
     private $headVars   = array();
     private $outputVars = array();
+    private $viewName   = null;
+    private $onlyTemplate = false;
 
     public function
     Prepare($action, $params)
     {
-        if( !method_exists($this, $action) )
+        if (!method_exists($this, $action))
         {
             trigger_error("unknown action: $action");
             exit();
@@ -51,7 +53,7 @@ class ComponentBase
     {
         $headVarString = '';
 
-        if( !isset($this->headVars['title']) )
+        if (!isset($this->headVars['title']))
         {
             $this->headVars['title'] = SiteConfig::DEFAULT_HEAD_TITLE;
         }
@@ -68,5 +70,29 @@ class ComponentBase
     GetExposedVariables()
     {
         return $this->outputVars;
+    }
+
+    public function
+    SetViewName($viewName)
+    {
+        $this->viewName = $viewName;
+    }
+
+    public function
+    GetViewName()
+    {
+        return $this->viewName;
+    }
+
+    public function
+    SetOnlyTemplate($onlyTemplate)
+    {
+        $this->onlyTemplate = $onlyTemplate;
+    }
+
+    public function
+    GetOnlyTemplate()
+    {
+        return $this->onlyTemplate;
     }
 }
